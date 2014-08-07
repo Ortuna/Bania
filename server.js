0cdfc9fd7fdc7b52c9d54f6595ac36813ca4c671
Sumeet Singh <ortuna@gmail.com>
2014-08-06 20:47:30 -0400

    Saving changes

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var app     = express();
+
+var express  = require('express'),
+    app      = express(),
+    fs       = require('fs'),
+    Calendar = require('./lib/calendar')
+
+ 
+var calendar = Calendar();    
+filePath = calendar.generate({});
 
 
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
-});
+
+/*app.get('/', function(req, res){*/
+  //res.send('Hello World');
+//});
 
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port %d', server.address().port);
-});
+//var server = app.listen(3000, function() {
+  //console.log('Listening on port %d', server.address().port);
+/*});*/
