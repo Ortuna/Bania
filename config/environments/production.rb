9be61447f4c91ed9423448e0b7314abb04d2ac5f
Sumeet Singh <ortuna@gmail.com>
2014-09-02 07:08:29 -0400

    Added correct SMTP stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
-    :address => "smtp.mailgun.org",
-    :port => 587,
-    :domain => "app28277986.mailgun.org",
-    :user_name => "postmaster@app28277986.mailgun.org",
-    :password => "4lgz3v4x3-p8"
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :domain         => "bania.io",
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD']
   }
 end
